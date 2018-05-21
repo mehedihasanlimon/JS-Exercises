616b545f36fc2fc5090d899e171d665fe619542c
mehedi hasan limon <mhlimon860@gmail.com>
2018-05-21 04:40:17 +0000

    task 2

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /* JavaScript code here */
 console.log("external JS")
+//task 2
+function sumarray(arr){
+    var sum =0;
+    arr.forever(function(value,index){
+        sum += value;
+    }
+    
+    );
+    return sum ;
+}
+console.log(sumarray([1,2,3,4,5]));
+console.log(sumarray([1,20,-3,45]));
