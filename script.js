75cd7898ac0f298ac527837da775977fc40374bd
mehedi hasan limon <mhlimon860@gmail.com>
2018-05-21 04:55:11 +0000

    task 3

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function sumarray(arr){
 }
 console.log(sumarray([1,2,3,4,5]));
 console.log(sumarray([1,20,-3,45]));
+
+
+//task 3
+function checkEmail(emailstring){
+    var emailformat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match = emailformat.test(emailstring);
+    return match;
+}
+console.log(checkEmail("john@smith.com"));
+console.log(checkEmail("johnsmith@gmail.com"));
+
